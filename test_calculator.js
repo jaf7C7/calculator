48e108a8c0f5cc5ca2c0706647c03450cf99677c
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-12-17 18:19:34 +0000

    Refactoring

--- test_calculator.js
+++ test_calculator.js
@@ -1,9 +1,10 @@
 // https://nodejs.org/api/assert.html
 const assert = require("node:assert/strict");
-const Calculator = require("./calculator.js");
 
 // https://mochajs.org/#assertions
 describe("Calculator", () => {
+  const Calculator = require("./calculator.js");
+
   it("should have primary and secondary displays", () => {
     const calc = new Calculator();
     assert.equal(
