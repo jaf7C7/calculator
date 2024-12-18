acda7802fdc8199f51c1d02287fb6cf7398cd1eb
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-12-18 14:05:34 +0000

    Extract setup hook from tests

--- test_calculator.js
+++ test_calculator.js
@@ -4,22 +4,24 @@ const assert = require("node:assert/strict");
 // https://mochajs.org/#assertions
 describe("Calculator", () => {
   const Calculator = require("./calculator.js");
+  let calc;
+
+  beforeEach(() => {
+    calc = new Calculator();
+  });
 
   it("should have displays which are initially blank", () => {
-    const calc = new Calculator();
     assert(calc.primaryDisplay === "" && calc.secondaryDisplay === "");
   });
 
   // ., 0, 1, ..., 9  (input keys)
   it("should display the correct characters when input keys are pressed", () => {
-    const calc = new Calculator();
     calc.inputChar("1");
     assert(calc.primaryDisplay === "1");
   });
 
   // `inputChar` should fail if `char` is not matched by `[.0-9]`
   it("should throw an error if inputChar is called with an invalid character", () => {
-    const calc = new Calculator();
     assert.throws(
       () => {
         calc.inputChar("a");
@@ -30,7 +32,6 @@ describe("Calculator", () => {
 
   // Del
   it("should delete the last character of the current operand when the Del key is pressed", () => {
-    const calc = new Calculator();
     calc.primaryDisplay = "a";
     calc.deleteChar();
     assert(calc.primaryDisplay === "");
@@ -38,7 +39,6 @@ describe("Calculator", () => {
 
   // AC
   it("should clear both displays when the AC key is pressed", () => {
-    const calc = new Calculator();
     calc.primaryDisplay = "foo";
     calc.secondaryDisplay = "bar";
     calc.clearAll();
@@ -47,7 +47,6 @@ describe("Calculator", () => {
 
   // -, +, *, /  (operator keys)
   it("should display the selected operand and operator when an operation is selected", () => {
-    const calc = new Calculator();
     calc.primaryDisplay = "10";
     calc.secondaryDisplay = "";
     calc.selectOperation("+");
@@ -56,7 +55,6 @@ describe("Calculator", () => {
 
   // `selectOperation` should fail if `operator` is not matched by `[-+*/]`
   it("should throw an error if selectOperation is called with an invalid operator", () => {
-    const calc = new Calculator();
     assert.throws(
       () => {
         calc.selectOperation("x");
