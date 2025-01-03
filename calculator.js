7387a5f891544c0fef709567c8ff7e049d158dfb
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-03 01:13:51 +0000

    Change formatting, just use tabs

--- calculator.js
+++ calculator.js
@@ -1,111 +1,111 @@
 class Calculator {
-  #primaryDisplay;
-  #secondaryDisplay;
-  #currentOperand;
-  #previousOperand;
-  #calculationString;
-  #operators;
-  #operator;
+	#primaryDisplay;
+	#secondaryDisplay;
+	#currentOperand;
+	#previousOperand;
+	#calculationString;
+	#operators;
+	#operator;
 
-  constructor(primaryDisplay, secondaryDisplay) {
-    this.#primaryDisplay = primaryDisplay;
-    this.#secondaryDisplay = secondaryDisplay;
-    this.#currentOperand = "";
-    this.#previousOperand = "";
-    this.#calculationString = "";
-    this.#operator = "";
-  }
+	constructor(primaryDisplay, secondaryDisplay) {
+		this.#primaryDisplay = primaryDisplay;
+		this.#secondaryDisplay = secondaryDisplay;
+		this.#currentOperand = "";
+		this.#previousOperand = "";
+		this.#calculationString = "";
+		this.#operator = "";
+	}
 
-  #updateDisplay() {
-    this.#primaryDisplay.update(this.#currentOperand);
-    this.#secondaryDisplay.update(this.#calculationString);
-  }
+	#updateDisplay() {
+		this.#primaryDisplay.update(this.#currentOperand);
+		this.#secondaryDisplay.update(this.#calculationString);
+	}
 
-  #validateInputChar(char) {
-    if (RegExp(/[^.0-9]/).test(char)) {
-      throw new TypeError(`Illegal input: '${char}'`);
-    }
-  }
+	#validateInputChar(char) {
+		if (RegExp(/[^.0-9]/).test(char)) {
+			throw new TypeError(`Illegal input: '${char}'`);
+		}
+	}
 
-  inputChar(char) {
-    this.#validateInputChar(char);
-    this.#currentOperand += char;
-    this.#updateDisplay();
-  }
+	inputChar(char) {
+		this.#validateInputChar(char);
+		this.#currentOperand += char;
+		this.#updateDisplay();
+	}
 
-  deleteChar() {
-    this.#currentOperand = this.#currentOperand.slice(0, -1);
-    this.#updateDisplay();
-  }
+	deleteChar() {
+		this.#currentOperand = this.#currentOperand.slice(0, -1);
+		this.#updateDisplay();
+	}
 
-  clearAll() {
-    this.#currentOperand = "";
-    this.#previousOperand = "";
-    this.#operator = "";
-    this.#calculationString = "";
-    this.#updateDisplay();
-    this.#primaryDisplay.update("");
-    this.#secondaryDisplay.update("");
-  }
+	clearAll() {
+		this.#currentOperand = "";
+		this.#previousOperand = "";
+		this.#operator = "";
+		this.#calculationString = "";
+		this.#updateDisplay();
+		this.#primaryDisplay.update("");
+		this.#secondaryDisplay.update("");
+	}
 
-  #newOperand() {
-    this.#previousOperand = this.#currentOperand;
-    this.#currentOperand = "";
-    this.#calculationString = this.#previousOperand;
-    if (this.#operator) {
-      this.#calculationString += this.#operator;
-    }
-  }
+	#newOperand() {
+		this.#previousOperand = this.#currentOperand;
+		this.#currentOperand = "";
+		this.#calculationString = this.#previousOperand;
+		if (this.#operator) {
+			this.#calculationString += this.#operator;
+		}
+	}
 
-  #validateOperator(operator) {
-    if (RegExp(/[^-+*/]/).test(operator)) {
-      throw new TypeError(`Illegal operator: '${operator}'`);
-    }
-  }
+	#validateOperator(operator) {
+		if (RegExp(/[^-+*/]/).test(operator)) {
+			throw new TypeError(`Illegal operator: '${operator}'`);
+		}
+	}
 
-  #chainingCalculations() {
-    return this.#operator && this.#previousOperand && this.#currentOperand;
-  }
+	#chainingCalculations() {
+		return this.#operator && this.#previousOperand && this.#currentOperand;
+	}
 
-  selectOperation(operator) {
-    this.#validateOperator(operator);
-    if (this.#chainingCalculations()) {
-      this.calculate();
-      this.#currentOperand = this.#previousOperand;
-    }
-    if (this.#currentOperand) {
-      this.#operator = operator;
-      this.#newOperand();
-    }
-    this.#updateDisplay();
-  }
+	selectOperation(operator) {
+		this.#validateOperator(operator);
+		if (this.#chainingCalculations()) {
+			this.calculate();
+			this.#currentOperand = this.#previousOperand;
+		}
+		if (this.#currentOperand) {
+			this.#operator = operator;
+			this.#newOperand();
+		}
+		this.#updateDisplay();
+	}
 
-  calculate() {
-    let result;
-    const [a, b] = [
-      Number(this.#previousOperand),
-      Number(this.#currentOperand),
-    ];
-    switch (this.#operator) {
-      case "+":
-        result = a + b;
-        break;
-      case "*":
-        result = a * b;
-        break;
-      case "/":
-        result = a / b;
-        break;
-      case "-":
-        result = a - b;
-        break;
-    }
-    this.#calculationString += this.#currentOperand;
-    this.#currentOperand = String(result);
-    this.#updateDisplay();
-    this.#operator = "";
-    this.#newOperand();
-  }
+	calculate() {
+		let result;
+		const [a, b] = [
+			Number(this.#previousOperand),
+			Number(this.#currentOperand),
+		];
+		switch (this.#operator) {
+			case "+":
+				result = a + b;
+				break;
+			case "*":
+				result = a * b;
+				break;
+			case "/":
+				result = a / b;
+				break;
+			case "-":
+				result = a - b;
+				break;
+		}
+		this.#calculationString += this.#currentOperand;
+		this.#currentOperand = String(result);
+		this.#updateDisplay();
+		this.#operator = "";
+		this.#newOperand();
+	}
 }
 
 module.exports = Calculator;
--- test-calculator.js
+++ test-calculator.js
@@ -2,192 +2,192 @@ const assert = require("node:assert/strict");
 const { describe, it, beforeEach } = require("node:test");
 
 describe("Calculator", () => {
-  const Calculator = require("./calculator.js");
-  let calc;
-  let primaryDisplay;
-  let secondaryDisplay;
+	const Calculator = require("./calculator.js");
+	let calc;
+	let primaryDisplay;
+	let secondaryDisplay;
 
-  class mockDisplay {
-    #content;
+	class mockDisplay {
+		#content;
 
-    constructor() {
-      this.#content = "";
-    }
+		constructor() {
+			this.#content = "";
+		}
 
-    update(str) {
-      this.#content = str;
-    }
+		update(str) {
+			this.#content = str;
+		}
 
-    read() {
-      return this.#content;
-    }
-  }
+		read() {
+			return this.#content;
+		}
+	}
 
-  beforeEach(() => {
-    primaryDisplay = new mockDisplay();
-    secondaryDisplay = new mockDisplay();
-    calc = new Calculator(primaryDisplay, secondaryDisplay);
-  });
+	beforeEach(() => {
+		primaryDisplay = new mockDisplay();
+		secondaryDisplay = new mockDisplay();
+		calc = new Calculator(primaryDisplay, secondaryDisplay);
+	});
 
-  it("should have displays which are initially blank", () => {
-    assert.equal(primaryDisplay.read(), "");
-    assert.equal(secondaryDisplay.read(), "");
-  });
+	it("should have displays which are initially blank", () => {
+		assert.equal(primaryDisplay.read(), "");
+		assert.equal(secondaryDisplay.read(), "");
+	});
 
-  describe("inputChar()", () => {
-    it("should append the input character to the display", () => {
-      calc.inputChar("1");
-      assert.equal(primaryDisplay.read(), "1");
-    });
+	describe("inputChar()", () => {
+		it("should append the input character to the display", () => {
+			calc.inputChar("1");
+			assert.equal(primaryDisplay.read(), "1");
+		});
 
-    it("should throw an error if the input character is not matched by '[.0-9]'", () => {
-      assert.throws(
-        () => {
-          calc.inputChar("a");
-        },
-        { name: "TypeError", message: "Illegal input: 'a'" },
-      );
-    });
+		it("should throw an error if the input character is not matched by '[.0-9]'", () => {
+			assert.throws(
+				() => {
+					calc.inputChar("a");
+				},
+				{ name: "TypeError", message: "Illegal input: 'a'" },
+			);
+		});
 
-    it("should correctly update the display after consecutive calculations", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.calculate();
-      calc.inputChar("1");
-      assert.equal(primaryDisplay.read(), "1");
-      assert.equal(secondaryDisplay.read(), "2");
-    });
-  });
+		it("should correctly update the display after consecutive calculations", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.calculate();
+			calc.inputChar("1");
+			assert.equal(primaryDisplay.read(), "1");
+			assert.equal(secondaryDisplay.read(), "2");
+		});
+	});
 
-  describe("deleteChar()", () => {
-    it("should delete the last character of the current operand", () => {
-      calc.inputChar("1");
-      calc.deleteChar();
-      assert.equal(primaryDisplay.read(), "");
-    });
+	describe("deleteChar()", () => {
+		it("should delete the last character of the current operand", () => {
+			calc.inputChar("1");
+			calc.deleteChar();
+			assert.equal(primaryDisplay.read(), "");
+		});
 
-    it("should do nothing if the current operand is empty", () => {
-      calc.deleteChar();
-      assert.equal(primaryDisplay.read(), "");
-    });
+		it("should do nothing if the current operand is empty", () => {
+			calc.deleteChar();
+			assert.equal(primaryDisplay.read(), "");
+		});
 
-    it("should correctly update the display after consecutive calculations", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.calculate();
-      calc.deleteChar();
-      assert.equal(primaryDisplay.read(), "");
-      assert.equal(secondaryDisplay.read(), "2");
-    });
-  });
+		it("should correctly update the display after consecutive calculations", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.calculate();
+			calc.deleteChar();
+			assert.equal(primaryDisplay.read(), "");
+			assert.equal(secondaryDisplay.read(), "2");
+		});
+	});
 
-  describe("selectOperation()", () => {
-    it("should append the selected operand and operator to the secondary display", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      assert.equal(secondaryDisplay.read(), "1+");
-      assert.equal(primaryDisplay.read(), "");
-    });
+	describe("selectOperation()", () => {
+		it("should append the selected operand and operator to the secondary display", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			assert.equal(secondaryDisplay.read(), "1+");
+			assert.equal(primaryDisplay.read(), "");
+		});
 
-    it("should throw an error if the selected operator is not matched by '[-+*/]'", () => {
-      assert.throws(
-        () => {
-          calc.selectOperation("x");
-        },
-        { name: "TypeError", message: "Illegal operator: 'x'" },
-      );
-    });
+		it("should throw an error if the selected operator is not matched by '[-+*/]'", () => {
+			assert.throws(
+				() => {
+					calc.selectOperation("x");
+				},
+				{ name: "TypeError", message: "Illegal operator: 'x'" },
+			);
+		});
 
-    it("should do nothing if it is called without an initial operand", () => {
-      calc.selectOperation("+");
-      assert.equal(primaryDisplay.read(), "");
-      assert.equal(secondaryDisplay.read(), "");
-    });
+		it("should do nothing if it is called without an initial operand", () => {
+			calc.selectOperation("+");
+			assert.equal(primaryDisplay.read(), "");
+			assert.equal(secondaryDisplay.read(), "");
+		});
 
-    it("should correctly update the display after consecutive calculations", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.calculate();
-      calc.selectOperation("+");
-      assert.equal(primaryDisplay.read(), "");
-      assert.equal(secondaryDisplay.read(), "2");
-    });
+		it("should correctly update the display after consecutive calculations", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.calculate();
+			calc.selectOperation("+");
+			assert.equal(primaryDisplay.read(), "");
+			assert.equal(secondaryDisplay.read(), "2");
+		});
 
-    it("should correctly handle calculation chaining", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.selectOperation("*");
-      assert.equal(primaryDisplay.read(), "");
-      assert.equal(secondaryDisplay.read(), "2*");
-    });
-  });
+		it("should correctly handle calculation chaining", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.selectOperation("*");
+			assert.equal(primaryDisplay.read(), "");
+			assert.equal(secondaryDisplay.read(), "2*");
+		});
+	});
 
-  describe("clearAll()", () => {
-    it("should clear the primary display", () => {
-      calc.inputChar("1");
-      assert.notEqual(primaryDisplay.read(), "");
-      calc.clearAll();
-      assert.equal(primaryDisplay.read(), "");
-    });
+	describe("clearAll()", () => {
+		it("should clear the primary display", () => {
+			calc.inputChar("1");
+			assert.notEqual(primaryDisplay.read(), "");
+			calc.clearAll();
+			assert.equal(primaryDisplay.read(), "");
+		});
 
-    it("should clear the secondary display", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      assert.notEqual(secondaryDisplay.read(), "");
-      calc.clearAll();
-      assert.equal(secondaryDisplay.read(), "");
-    });
+		it("should clear the secondary display", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			assert.notEqual(secondaryDisplay.read(), "");
+			calc.clearAll();
+			assert.equal(secondaryDisplay.read(), "");
+		});
 
-    it("should correctly update the display after consecutive calculations", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.calculate();
-      calc.clearAll();
-      assert.equal(primaryDisplay.read(), "");
-      assert.equal(secondaryDisplay.read(), "");
-    });
-  });
+		it("should correctly update the display after consecutive calculations", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.calculate();
+			calc.clearAll();
+			assert.equal(primaryDisplay.read(), "");
+			assert.equal(secondaryDisplay.read(), "");
+		});
+	});
 
-  describe("calculate()", () => {
-    it("should display the calculation and its result", () => {
-      calc.inputChar("1");
-      calc.selectOperation("+");
-      calc.inputChar("1");
-      calc.calculate();
-      assert.equal(primaryDisplay.read(), "2");
-      assert.equal(secondaryDisplay.read(), "1+1");
-    });
+	describe("calculate()", () => {
+		it("should display the calculation and its result", () => {
+			calc.inputChar("1");
+			calc.selectOperation("+");
+			calc.inputChar("1");
+			calc.calculate();
+			assert.equal(primaryDisplay.read(), "2");
+			assert.equal(secondaryDisplay.read(), "1+1");
+		});
 
-    it("should handle multiplication", () => {
-      calc.inputChar("3");
-      calc.selectOperation("*");
-      calc.inputChar("3");
-      calc.calculate();
-      assert.equal(primaryDisplay.read(), "9");
-      assert.equal(secondaryDisplay.read(), "3*3");
-    });
+		it("should handle multiplication", () => {
+			calc.inputChar("3");
+			calc.selectOperation("*");
+			calc.inputChar("3");
+			calc.calculate();
+			assert.equal(primaryDisplay.read(), "9");
+			assert.equal(secondaryDisplay.read(), "3*3");
+		});
 
-    it("should handle division", () => {
-      calc.inputChar("3");
-      calc.selectOperation("/");
-      calc.inputChar("3");
-      calc.calculate();
-      assert.equal(primaryDisplay.read(), "1");
-      assert.equal(secondaryDisplay.read(), "3/3");
-    });
+		it("should handle division", () => {
+			calc.inputChar("3");
+			calc.selectOperation("/");
+			calc.inputChar("3");
+			calc.calculate();
+			assert.equal(primaryDisplay.read(), "1");
+			assert.equal(secondaryDisplay.read(), "3/3");
+		});
 
-    it("should handle subtraction", () => {
-      calc.inputChar("3");
-      calc.selectOperation("-");
-      calc.inputChar("3");
-      calc.calculate();
-      assert.equal(primaryDisplay.read(), "0");
-      assert.equal(secondaryDisplay.read(), "3-3");
-    });
-  });
+		it("should handle subtraction", () => {
+			calc.inputChar("3");
+			calc.selectOperation("-");
+			calc.inputChar("3");
+			calc.calculate();
+			assert.equal(primaryDisplay.read(), "0");
+			assert.equal(secondaryDisplay.read(), "3-3");
+		});
+	});
 });
