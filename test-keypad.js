2737de4e38aad9b0a866e0826888bd9d9ef1d455
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-06 19:59:43 +0000

    Extract common setup code into setup function

--- test-keypad.js
+++ test-keypad.js
@@ -7,23 +7,22 @@ describe("Keypad()", () => {
 	let fakeCalc;
 
 	describe("Equals key", () => {
+		let equalsKey;
+
 		beforeEach(() => {
 			fakeCalc = {
 				calculate: mock.fn(),
 			};
+			kp = new Keypad(fakeCalc);
+			kp.addKey("=", "calculate");
+			[equalsKey] = kp.keys.filter((k) => k.value === "=");
 		});
 
 		it("Should have value `=`", () => {
-			kp = new Keypad(fakeCalc);
-			kp.addKey("=", "calculate");
-			const [equalsKey] = kp.keys.filter((k) => k.value === "=");
 			assert.notEqual(equalsKey, undefined);
 		});
 
 		it("Should call `calculate()` on the wrapped `Calculator` instance", () => {
-			kp = new Keypad(fakeCalc);
-			kp.addKey("=", "calculate");
-			const [equalsKey] = kp.keys.filter((k) => k.value === "=");
 			equalsKey.press();
 			assert.equal(fakeCalc.calculate.mock.callCount(), 1);
 		});
