6bdd786b411ea6d55e22d5598db12ac8fa57853e
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-06 21:23:08 +0000

    Remove unnecessary parameterizations

--- test-keypad.js
+++ test-keypad.js
@@ -3,34 +3,28 @@ const { describe, it, mock, beforeEach } = require("node:test");
 
 describe("Keypad()", () => {
 	const Keypad = require("./keypad.js");
-	const functionKeys = [
-		{ value: "=", onPress: "calculate" },
-		{ value: "Del", onPress: "deleteChar" },
-		{ value: "AC", onPress: "allClear" },
-	];
+	const functionKey = { value: "=", onPress: "calculate" };
 
-	functionKeys.forEach((functionKey) => {
-		describe(`Function key '${functionKey.value}'`, () => {
-			let kp;
-			let fakeCalc;
-			let key;
+	describe("addKey()", () => {
+		let kp;
+		let fakeCalc;
+		let key;
 
-			beforeEach(() => {
-				fakeCalc = {};
-				fakeCalc[functionKey.onPress] = mock.fn();
-				kp = new Keypad(fakeCalc);
-				kp.addKey(functionKey);
-				[key] = kp.keys.filter((k) => k.value === functionKey.value);
-			});
+		beforeEach(() => {
+			fakeCalc = {};
+			fakeCalc[functionKey.onPress] = mock.fn();
+			kp = new Keypad(fakeCalc);
+			kp.addKey(functionKey);
+			[key] = kp.keys.filter((k) => k.value === functionKey.value);
+		});
 
-			it(`Should have value '${functionKey.value}'`, () => {
-				assert.notEqual(key, undefined);
-			});
+		it("Should have the correct value", () => {
+			assert.notEqual(key, undefined);
+		});
 
-			it(`Should call '${functionKey.onPress}()' on the wrapped 'Calculator' instance`, () => {
-				key.press();
-				assert.equal(fakeCalc[functionKey.onPress].mock.callCount(), 1);
-			});
+		it(`"Should call the correct method on the wrapped 'Calculator' instance`, () => {
+			key.press();
+			assert.equal(fakeCalc[functionKey.onPress].mock.callCount(), 1);
 		});
 	});
 });
