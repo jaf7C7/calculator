a33c1ccdac5d440824834cc032c80301707a5c20
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-08 19:21:39 +0000

    Make `initApp` create the "0" key

--- app.test.js
+++ app.test.js
@@ -1,12 +1,19 @@
 const assert = require("node:assert/strict");
-const { describe, it, mock } = require("node:test");
+const { describe, it, mock, beforeEach } = require("node:test");
 
 describe("initApp()", () => {
 	const initApp = require("./app.js");
+	let fakeUI;
+
+	beforeEach(() => {
+		fakeUI = {
+			createKey: mock.fn(),
+			createDisplay: mock.fn(),
+		};
+		initApp(fakeUI);
+	});
 
 	it("Should create primary and secondary displays", () => {
-		const fakeUI = { createDisplay: mock.fn() };
-		initApp(fakeUI);
 		assert.equal(fakeUI.createDisplay.mock.callCount(), 2);
 		assert.equal(
 			fakeUI.createDisplay.mock.calls[0].arguments[0],
@@ -16,5 +23,10 @@ describe("initApp()", () => {
 			fakeUI.createDisplay.mock.calls[1].arguments[0],
 			"secondaryDisplay",
 		);
+	});
+
+	it("Should create an input key for number 0", () => {
+		assert.equal(fakeUI.createKey.mock.callCount(), 1);
+		assert.equal(fakeUI.createKey.mock.calls[0].arguments[0], "0");
 	});
 });
