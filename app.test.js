466d9b75cf79405ac9ec57aea4b98a0bc78b7030
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-10 20:50:47 +0000

    Refactor test to prepare for parameterisation

--- app.test.js
+++ app.test.js
@@ -56,8 +56,15 @@ describe("initApp()", () => {
 		});
 	});
 
-	it("Should create a 'Del' button to delete characters from the input", () => {
-		const button = fakeUI.getElement("btnDel");
-		assert.equal(button.id, "btnDel");
+	describe("Should create a 'Del' button to delete characters from the input", () => {
+		let button;
+
+		beforeEach(() => {
+			button = fakeUI.getElement("btnDel");
+		});
+
+		it("Should have the correct id", () => {
+			assert.equal(button.id, "btnDel");
+		});
 	});
 });
