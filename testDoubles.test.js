9069a32672c834748b642254dd3a78e23a81988c
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-10 19:08:05 +0000

    Refacor tests

--- testDoubles.test.js
+++ testDoubles.test.js
@@ -1,29 +1,37 @@
 const assert = require("node:assert");
-const { describe, it } = require("node:test");
+const { describe, it, beforeEach } = require("node:test");
 const { FakeDisplay, FakeUI } = require("./testDoubles.js");
 
 describe("FakeDisplay", () => {
+	let fakeDisplay;
+
+	beforeEach(() => {
+		fakeDisplay = new FakeDisplay();
+	});
+
 	it("Should read out the same text it was updated with", () => {
-		const fakeDisplay = new FakeDisplay();
 		fakeDisplay.update("hello");
 		assert.equal(fakeDisplay.read(), "hello");
 	});
 
 	it("Should return an empty string if it has not been updated", () => {
-		const fakeDisplay = new FakeDisplay();
 		assert.equal(fakeDisplay.read(), "");
 	});
 });
 
 describe("FakeUI", () => {
+	let fakeUI;
+
+	beforeEach(() => {
+		fakeUI = new FakeUI();
+	});
+
 	it("Should be able to create and fetch display elements", () => {
-		const fakeUI = new FakeUI();
 		fakeUI.createDisplay("someID");
 		assert.equal(fakeUI.getDisplay("someID").id, "someID");
 	});
 
 	it("Should be able to create and fetch button elements", () => {
-		const fakeUI = new FakeUI();
 		fakeUI.createButton("zeroButton", "0", () => "Click!");
 		const zeroButton = fakeUI.getButton("zeroButton");
 		assert.equal(zeroButton.id, "zeroButton");
