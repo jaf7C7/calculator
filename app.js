6e568ba6d429eb9c1595dfa95d44292c22bfdff7
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-10 14:00:54 +0000

    Remove references to old mockUI

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-function initApp(ui, new_ui) {
-	ui.createDisplay("primaryDisplay");
-	ui.createDisplay("secondaryDisplay");
+function initApp(new_ui) {
 	new_ui.createDisplay("primaryDisplay");
 	new_ui.createDisplay("secondaryDisplay");
 	new_ui.createButton("zeroButton", "0");
-	ui.createButton("0");
 }
 
 module.exports = initApp;
--- app.test.js
+++ app.test.js
@@ -1,31 +1,14 @@
 const assert = require("node:assert/strict");
-const { describe, it, mock, beforeEach } = require("node:test");
+const { describe, it, beforeEach } = require("node:test");
 
 describe("initApp()", () => {
 	const initApp = require("./app.js");
 	const { FakeUI } = require("./testDoubles.js");
-	let mockUI;
 	let fakeUI;
 
 	beforeEach(() => {
-		mockUI = {
-			createButton: mock.fn(),
-			createDisplay: mock.fn(),
-		};
 		fakeUI = new FakeUI();
-		initApp(mockUI, fakeUI);
-	});
-
-	it("Should create primary and secondary displays", () => {
-		assert.equal(mockUI.createDisplay.mock.callCount(), 2);
-		assert.equal(
-			mockUI.createDisplay.mock.calls[0].arguments[0],
-			"primaryDisplay",
-		);
-		assert.equal(
-			mockUI.createDisplay.mock.calls[1].arguments[0],
-			"secondaryDisplay",
-		);
+		initApp(fakeUI);
 	});
 
 	it("Should create primary and secondary displays using the new fakeUI", () => {
@@ -38,10 +21,5 @@ describe("initApp()", () => {
 	it("Should create an input button for number 0 using the new fakeUI", () => {
 		const zeroButton = fakeUI.getButton("zeroButton");
 		assert.equal(zeroButton.value, "0");
-	});
-
-	it("Should create an input button for number 0", () => {
-		assert.equal(mockUI.createButton.mock.callCount(), 1);
-		assert.equal(mockUI.createButton.mock.calls[0].arguments[0], "0");
 	});
 });
