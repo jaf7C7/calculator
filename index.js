c05046d707ce02c6398061d2dc6048e122b5770f
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-03-16 23:57:00 +0000

    Encapsulate variables in wrapper function

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-const container = document.getElementById("calculator");
+function createApp() {
+	const container = document.getElementById("calculator");
 
-function createElement(tagName, id, textContent = null, onClick = null) {
-	const element = document.createElement(tagName);
-	element.id = id;
-	if (textContent) {
-		element.textContent = textContent;
-	}
-	if (onClick) {
-		element.addEventListener("click", onClick);
-	}
-	container.appendChild(element);
-	return element;
-}
-
-function createButton(id, value, onClick) {
-	createElement("button", id, value, onClick);
-}
-
-let firstOperand;
-let secondOperand;
-let operation;
-const display = createElement("div", "display");
-
-function createInputButton(id, value) {
-	createButton(id, value, () => {
-		if (!firstOperand) {
-			firstOperand = value;
-		} else {
-			secondOperand = value;
+	function createElement(tagName, id, textContent = null, onClick = null) {
+		const element = document.createElement(tagName);
+		element.id = id;
+		if (textContent) {
+			element.textContent = textContent;
 		}
-		display.textContent += String(value);
+		if (onClick) {
+			element.addEventListener("click", onClick);
+		}
+		container.appendChild(element);
+		return element;
+	}
+
+	function createButton(id, value, onClick) {
+		createElement("button", id, value, onClick);
+	}
+
+	let firstOperand;
+	let secondOperand;
+	let operation;
+	const display = createElement("div", "display");
+
+	function createInputButton(id, value) {
+		createButton(id, value, () => {
+			if (!firstOperand) {
+				firstOperand = value;
+			} else {
+				secondOperand = value;
+			}
+			display.textContent += String(value);
+		});
+	}
+
+	const inputButtons = [
+		["one", 1],
+		["two", 2],
+		["five", 5],
+	];
+
+	inputButtons.forEach(([id, value]) => {
+		createInputButton(id, value);
+	});
+
+	function createOperationButton(id, value, _operation) {
+		createButton(id, value, () => {
+			operation = _operation;
+			display.textContent += value;
+		});
+	}
+
+	const operationButtons = [
+		["plus", "+", (a, b) => a + b],
+		["times", "*", (a, b) => a * b],
+	];
+
+	operationButtons.forEach(([id, value, _operation]) => {
+		createOperationButton(id, value, _operation);
+	});
+
+	createButton("equals", "=", () => {
+		display.textContent = operation(firstOperand, secondOperand);
 	});
 }
 
-const inputButtons = [
-	["one", 1],
-	["two", 2],
-	["five", 5],
-];
-
-inputButtons.forEach(([id, value]) => {
-	createInputButton(id, value);
-});
-
-function createOperationButton(id, value, _operation) {
-	createButton(id, value, () => {
-		operation = _operation;
-		display.textContent += value;
-	});
-}
-
-const operationButtons = [
-	["plus", "+", (a, b) => a + b],
-	["times", "*", (a, b) => a * b],
-];
-
-operationButtons.forEach(([id, value, _operation]) => {
-	createOperationButton(id, value, _operation);
-});
-
-createButton("equals", "=", () => {
-	display.textContent = operation(firstOperand, secondOperand);
-});
+createApp();
