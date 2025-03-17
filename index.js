3a4c68cca173c598e015f1b69878d389131edacd
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-03-17 00:26:53 +0000

    Slide variables

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ function createButton(container, id, value, onClick) {
 }
 
 function createApp() {
-	const container = document.getElementById("calculator");
-	const display = createElement(container, "div", "display");
-
 	let firstOperand;
 	let secondOperand;
 	let operation;
@@ -34,16 +31,6 @@ function createApp() {
 		});
 	}
 
-	const inputButtons = [
-		["one", 1],
-		["two", 2],
-		["five", 5],
-	];
-
-	inputButtons.forEach(([id, value]) => {
-		createInputButton(container, display, id, value);
-	});
-
 	function createOperationButton(container, display, id, value, _operation) {
 		createButton(container, id, value, () => {
 			operation = _operation;
@@ -51,20 +38,33 @@ function createApp() {
 		});
 	}
 
-	const operationButtons = [
-		["plus", "+", (a, b) => a + b],
-		["times", "*", (a, b) => a * b],
-	];
-
-	operationButtons.forEach(([id, value, _operation]) => {
-		createOperationButton(container, display, id, value, _operation);
-	});
-
 	function createEqualsButton(container, display) {
 		createButton(container, "equals", "=", () => {
 			display.textContent = operation(firstOperand, secondOperand);
 		});
 	}
+
+	const container = document.getElementById("calculator");
+	const display = createElement(container, "div", "display");
+
+	const inputButtons = [
+		["one", 1],
+		["two", 2],
+		["five", 5],
+	];
+
+	const operationButtons = [
+		["plus", "+", (a, b) => a + b],
+		["times", "*", (a, b) => a * b],
+	];
+
+	inputButtons.forEach(([id, value]) => {
+		createInputButton(container, display, id, value);
+	});
+
+	operationButtons.forEach(([id, value, _operation]) => {
+		createOperationButton(container, display, id, value, _operation);
+	});
 
 	createEqualsButton(container, display);
 }
