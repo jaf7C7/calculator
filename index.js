3f1813464f03f702a46e8d9a99f90580f679563a
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-03-17 20:14:53 +0000

    Rename function

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function selectOperation(display, calculation, operation, value) {
 	display.append(value);
 };
 
-function performCalculation(display, calculation) {
+function calculate(display, calculation) {
 	display.update(calculation.calculate());
 };
 
@@ -114,7 +114,7 @@ function createApp() {
 	});
 
 	container.createButton("equals", "=", () => {
-		performCalculation(display, calculation);
+		calculate(display, calculation);
 	});
 }
 
