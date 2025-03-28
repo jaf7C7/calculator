ede9d02c127509521d96d314072f3f8ddf474c69
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-03-28 22:51:56 +0000

    Make button definitions more explicit

--- index.js
+++ index.js
@@ -156,63 +156,85 @@ function createApp() {
 		{
 			id: "one",
 			value: "1",
+			function: "input",
+			args: ["1"],
 			keybindings: [{value: "1", ctrlKey: false}],
 		},
 		{
 			id: "two",
 			value: "2",
+			function: "input",
+			args: ["2"],
 			keybindings: [{value: "2", ctrlKey: false}],
 		},
 		{
 			id: "three",
 			value: "3",
+			function: "input",
+			args: ["3"],
 			keybindings: [{value: "3", ctrlKey: false}],
 		},
 		{
 			id: "four",
 			value: "4",
+			function: "input",
+			args: ["4"],
 			keybindings: [{value: "4", ctrlKey: false}],
 		},
 		{
 			id: "five",
 			value: "5",
+			function: "input",
+			args: ["5"],
 			keybindings: [{value: "5", ctrlKey: false}],
 		},
 		{
 			id: "six",
 			value: "6",
+			function: "input",
+			args: ["6"],
 			keybindings: [{value: "6", ctrlKey: false}],
 		},
 		{
 			id: "seven",
 			value: "7",
+			function: "input",
+			args: ["7"],
 			keybindings: [{value: "7", ctrlKey: false}],
 		},
 		{
 			id: "eight",
 			value: "8",
+			function: "input",
+			args: ["8"],
 			keybindings: [{value: "8", ctrlKey: false}],
 		},
 		{
 			id: "nine",
 			value: "9",
+			function: "input",
+			args: ["9"],
 			keybindings: [{value: "9", ctrlKey: false}],
 		},
 		{
 			id: "zero",
 			value: "0",
+			function: "input",
+			args: ["0"],
 			keybindings: [{value: "0", ctrlKey: false}],
 		},
 		{
 			id: "point",
 			value: ".",
+			function: "input",
+			args: ["."],
 			keybindings: [{value: ".", ctrlKey: false}],
 		},
 	]
 
 	inputButtons.forEach((btn) => {
 		ui.createButton(btn.id, btn.value, () => {
-			calculator.input(btn.value);
+			calculator[btn.function](...btn.args);
 		}, btn.keybindings);
 	});
 
